e79ddcdce297bfa6c59039485822bc70b6173924
JoseXFlores <jose.xflores88@gmail.com>
2025-05-19 09:04:54 -0600

    base

---
+++ API Rest con microservicios/commons-microservicio/src/main/java/com/api/commons/servicies/CommonServiceImpl.java
@@ -0,0 +1,58 @@
+package com.api.commons.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.commons.mappers.CommonMapper;
+
+public class CommonServiceImpl<D, E, 
+M extends CommonMapper <D,E,R>,
+R extends JpaRepository<E, Long>> implements CommonService<D> {
+	
+	@Autowired
+	protected R repository;
+	
+	@Autowired
+	protected M mapper;
+
+	@Override
+	public List<D> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<D> obtenerPorId(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public D editar(D dto, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public D insertar(D dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public D eliminar(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
